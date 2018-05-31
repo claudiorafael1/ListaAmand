25ae65a366e4bd9eae63077478b13a2eab088ae2
claudiorafael1 <claudio-rafael@outlook.com>
2018-05-30 21:29:33 -0300

    Update Atividade30.c
    Versao Completa

--- ListaAmand/Atividade30Refazer/Atividade30.c
+++ ListaAmand/Atividade30Refazer/Atividade30.c
@@ -1,3 +1,24 @@
-#include <stdio.h>
-#include <stdlib.h>
-int main(){	}
+int num(int x){
+  int i, j , aux ,primo = 0,p;
+  printf("ponto aaa\n");
+  if (x == 1)
+     return 1;
+  for (i = 1; i<=x; i++) {
+    aux  = 0;
+    for (j = 1 ; j <=i; j++) {
+        if (i % j == 0)
+            aux += 1;
+    }
+    if (aux == 2)
+      primo += i;
+  }
+  return primo+1; //soma 1 porque 1 é primo e ta entre
+}
+
+int main(){
+    int g,f;
+    printf("Digite um numero :\n");
+    scanf("%d",&g);
+    f = num(g);
+    printf("%d :\n",f);
+}
