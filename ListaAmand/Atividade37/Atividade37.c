0dd4666875802f622fcbf4964662bc22ff0cff67
claudiorafael1 <claudio-rafael@outlook.com>
2018-05-31 22:08:32 -0300

    Update Atividade37.c

--- ListaAmand/Atividade37/Atividade37.c
+++ ListaAmand/Atividade37/Atividade37.c
@@ -1,7 +1,29 @@
-#include<stdio.h>
-int main(){
+/*1 - Crie uma fun��o que recebe uma string e um caractere, e apague todas as ocorr�ncias
+desses caractere na string.*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+main(){
+	int cont;
+	char palavra[20],compara;
+   setlocale (LC_ALL,"Portuguese");
+   printf("Escreva a palavra: ");
+   fgets(palavra,15,stdin);
 
-printf("Digite uma palavra com no maximo 15 caracteres:\n");
+   printf("Agora escreva um caractere que vc quer comparar: ");
+   scanf("%c",&compara);
 
-  return 0;
+
+   for (int i = 0;i<strlen(palavra);i++){
+   	   if(palavra[i]== compara){
+   	   	 cont++;
+		  }
+	   if(cont>=1){
+	   	palavra[i]='_';
+	   }
+	   cont=0;
+   }
+printf("A palavra ficou %s\n",palavra);
+
 }
